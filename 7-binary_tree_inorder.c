68864a576e3fb6a9c1018f2f4253a4df63c3fee3
Tariq5mo <tariqomer756@gmail.com>
2024-10-10 12:16:30 +0300

    Fix task 7

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -10,23 +10,23 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
-	if (binary_tree_is_leaf(tree->left) == 0)
+	if (binary_tree_is_leaff(tree->left) == 0)
 		binary_tree_inorder(tree->left, func);
 	else
 		func(tree->left->n);
 	func(tree->n);
-	if (binary_tree_is_leaf(tree->right) == 0)
+	if (binary_tree_is_leaff(tree->right) == 0)
 		binary_tree_inorder(tree->right, func);
 	else
 		func(tree->right->n);
 }
 /**
- * binary_tree_is_leaf - Checks if a node is a leaf.
+ * binary_tree_is_leaff - Checks if a node is a leaf.
  *
  * @node: A pointer to the node to check.
  * Return: 1 if node is a leaf, otherwise 0.
  */
-int binary_tree_is_leaf(const binary_tree_t *node)
+int binary_tree_is_leaff(const binary_tree_t *node)
 {
 	if (node && !(node->left) && !(node->right))
 		return (1);
